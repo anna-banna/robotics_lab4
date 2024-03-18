460bb913fb713edbd1ee2c7bc9ee76731336a772
anna-banna <aburgess1098@gmail.com>
2024-03-18 22:33:50 +0000

    adjusted mask and overlay

--- scripts/detect_ball.py
+++ scripts/detect_ball.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+# based on Dr. Saeidi's flip_image.py node
 import rospy
 import numpy as np
 import cv2
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
+# set up flag
 img_received = False
 # define a 720x1280 3-channel image with all pixels equal to zero
 rgb_img = np.zeros((720, 1280, 3), dtype = "uint8")
-
 
 # get the image message
 def get_image(ros_img):
@@ -20,11 +21,26 @@ def get_image(ros_img):
 	img_received = True
 	
 def detect_ball(image): 
-	lower_yellow_rgb = np.array([100,100,0])
-	upper_yellow_rgb = np.array([255,255,100])
-	yellow_mask = cv2.inRange(image, lower_yellow_rgb, upper_yellow_rgb)
+
+	# convert the image that's passed into the detect_ball function to HSV space
+	hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 	
-	return yellow_mask
+	# create rectangle that's the same size as the image we pass
+	rectangle = np.zeros((720, 1280, 1), dtype="uint8")
+	# use OpenCV function "rectangle" to overlay region of interest
+	cv2.rectangle(rectangle, (200,200), (850,500) , 255, -1)
+	
+	# create the hsv upper and lowers bounds for color filtering
+	lower_yellow_hsv = np.array([22,4,10])
+	upper_yellow_hsv = np.array([60,255,255])
+	# apply the mask
+	yellow_mask = cv2.inRange(hsv, lower_yellow_hsv, upper_yellow_hsv)
+	
+	# apply overlay
+	overlay_hsv = cv2.bitwise_and(rectangle, yellow_mask)
+	
+	# return maskes and overlayed image
+	return overlay_hsv
 
 	
 if __name__ == '__main__':
@@ -42,6 +58,7 @@ if __name__ == '__main__':
 		# make sure we process if the camera has started streaming images
 		if img_received:
 			
+			# apply the function to color filter the image
 			mono_ball = detect_ball(rgb_img)
 			
 			# convert it to ros msg and publish it
